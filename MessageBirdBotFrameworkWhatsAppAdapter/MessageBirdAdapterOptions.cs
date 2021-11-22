1df0ae63020c8c234162a7ff640b803726c7f38b
ahmet kocadoğan <ahmetkocadogan@gmail.com>
2021-11-22 11:34:57 +0300

    Add project files.

---
+++ MessageBirdBotFrameworkWhatsAppAdapter/MessageBirdAdapterOptions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MessageBirdBotFrameworkWhatsAppAdapter
+{
+    public class MessageBirdAdapterOptions
+    {
+        public MessageBirdAdapterOptions(IConfiguration configuration)
+            : this(configuration["MessageBirdAccessKey"], configuration["MessageBirdSigningKey"], Convert.ToBoolean(configuration["MessageBirdDisableRequestVerification"]))
+        { }
+
+        public MessageBirdAdapterOptions(string accessKey, string signingKey, bool disableRequestVerification)
+        {
+            AccessKey = accessKey;
+            SigningKey = signingKey;
+            DisableRequestVerification = disableRequestVerification;
+        }
+        public string AccessKey { get; set; }
+        public string SigningKey { get; set; }
+        public bool DisableRequestVerification { get; set; }
+    }
+}
---
+++ MessageBirdBotFrameworkWhatsAppAdapter/Models/MessageBirdSendMessagePayload.cs
@@ -0,0 +1,13 @@
+﻿using MessageBird.Objects.Conversations;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MessageBirdBotFrameworkWhatsAppAdapter.Models
+{
+    public class MessageBirdSendMessagePayload
+    {
+        public string conversationId { get; set; }
+        public ConversationMessageSendRequest conversationMessageRequest { get; set; }
+    }
+}
